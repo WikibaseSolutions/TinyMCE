55c004c83f31c5145347b366c6cf87da8278eb14
AceTMWC <56593048+AceTMWC@users.noreply.github.com>
2021-05-19 15:24:53 +0200

    use the PastePostProcess event to not get wikilink placeholders inside ws_link placeholders

--- custom_plugins/wikibase/plugins/ws_paste/plugin.js
+++ custom_plugins/wikibase/plugins/ws_paste/plugin.js
@@ -19,27 +19,44 @@ var Ws_Paste = function (editor) {
         var $html = $(html);
         if ( $($html).find('span.mwt-ws-non-editable').length > 0 ) {
             $($html).find('span[data-wiki]').parent().replaceWith(function (i) {
-                var newHt = $(this).find('span[data-wiki]').attr('data-wiki');
+                const newHt = $(this).find('span[data-wiki]').attr('data-wiki');
                 return convertBack2Wiki(newHt);
             });
         } else {
             $($html).find('span[data-wiki]').replaceWith(function (i) {
-                var newHt = $(this).attr('data-wiki');
-                return convertBack2Wiki(newHt);
+                const newHt = $(this).attr('data-wiki');
+                return  convertBack2Wiki(newHt);
             });
         }
-        console.log($html);
+
         $html = _removeUnWantedClasses($html);
         $html = _removeUnWantedMarkJSSpans($html);
         html = $($html).html();
         var textObject = {text: html};
         $(document).trigger('TinyMCEBeforeWikiToHtml', [textObject]);
         html = textObject.text;
-
-        $(document).trigger('TinyMCEAfterWikiToHtml', [textObject]);
-        html = textObject.text;
+        // console.log(html);
+        // $(document).trigger('TinyMCEAfterWikiToHtml', [textObject]);
+        // html = textObject.text;
         data.text = html;
         return data;
+    }
+
+    function _pasteAfterProcess(e, data) {
+        // console.log(data.text);
+        var textObject = {text: data.text};
+        $(document).trigger('TinyMCEAfterWikiToHtml', [textObject]);
+        // console.log(textObject.text);
+        data.text = textObject.text;
+        return data;
+    }
+
+    function _pastePostProcess(e) {
+        const $elm = $(e.node);
+        var textObject = {text: $elm.html()};
+        $(document).trigger('TinyMCEAfterWikiToHtml', [textObject]);
+        $(e.node).html(textObject.text);
+        return e;
     }
 
     function convertBack2Wiki(txt) {
@@ -48,6 +65,7 @@ var Ws_Paste = function (editor) {
 
     this.init = function (ed, url) {
         $(document).on('TinyMCEBeforePastePreProcess', _pastePreProcess);
+        ed.on('PastePostProcess', _pastePostProcess);
     }
 
 
