9a59679522989bc8380f988132290cc97f75f172
aoxomoxoa <duncan.crane@aoxomoxoa.co.uk>
2022-05-22 11:18:44 +0100

    Fix for spaces pasted at start of line
    When pasting in TinyMCE at the start of a paragraph, if there is a space at the start of the pasted text, the pated text is interpreted by mediawiki as a psuedo 'pre' block.  This is how mediawiki interprets text on a line preceded by a space character.
    To avoid this, the space must be replaced by a non-breaking space character '&nbsp;'.

--- custom_plugins/mediawiki/plugins/mw_wikiparser/plugin.js
+++ custom_plugins/mediawiki/plugins/mw_wikiparser/plugin.js
@@ -4449,10 +4449,9 @@
 		var text,
 			dom;
 
-
 		dom = $(e.node);
 		text = dom[0].innerHTML;
-		
+
 		// get rid of any empty spans - these result from drag drop
 		// operations where computed styles have been filtered out
 		dom.find( "span" ).replaceWith( function() {
@@ -4472,6 +4471,8 @@
 		} else {
 			_externalPaste( dom );
 		}
+
+		dom[0].innerHTML = dom[0].innerHTML.replace(/\<span\>\&nbsp\;\<\/span\>/ig, "&nbsp;");
 
 		text = dom[0].innerHTML;
 		debug( editor, "postOnPastePostProcess", _mwtDebugFlags.postOnPastePreProcess, text );
@@ -4677,7 +4678,7 @@
 				nextNode: nextNode
 			}
 		} 
-	
+
 		if ( evt.keyCode == 38 ) {
 			// up-arrow or down arrow at start or end of editor
 			// content results in an empty paragraph being added
