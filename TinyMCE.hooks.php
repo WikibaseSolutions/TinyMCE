8e4eafda562de1e56b6bda1601c321acf095afcb
Wout Gevaert <wout@wikibase.nl>
2025-05-07 12:13:35 +0200

    Add changes from 1_43 branch

--- TinyMCE.hooks.php
+++ TinyMCE.hooks.php
@@ -23,15 +23,6 @@ class TinyMCEHooks {
 
 		$GLOBALS['wgTinyMCEIP'] = dirname( __DIR__ ) . '/../';
 		$GLOBALS['wgTinyMCEUse'] = null;
-
-		// We have to have this hook called here, instead of in
-		// extension.json, because it's conditional.
-		if ( class_exists( 'MediaWiki\Linker\LinkRenderer' ) ) {
-			// MW 1.28+
-			$GLOBALS['wgHooks']['HtmlPageLinkRendererEnd'][] = 'TinyMCEHooks::changeRedLink';
-		} else {
-			$GLOBALS['wgHooks']['LinkEnd'][] = 'TinyMCEHooks::changeRedLinkOld';
-		}
 	}
 
 	/**
